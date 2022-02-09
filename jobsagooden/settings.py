86a5a15f9294001b57d72b9aa9381b1671ff562c
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-02-09 11:57:08 +0100

    Add file path to settings.py to for templates folder

--- jobsagooden/settings.py
+++ jobsagooden/settings.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'jobsagooden.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
