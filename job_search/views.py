a83e8a77722c031ceae3920e238f47d2290d8ee5
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-02-09 11:57:36 +0100

    Change HTTPresponse to render in views.py

--- job_search/views.py
+++ job_search/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 
 
 def job_search(request):
-    return HttpResponse('Here are the returned job posts')
+    return render(request, 'pages/job-search.html')
 
 
 def full_listing(request, pk):
-    return HttpResponse(f'Here is the full job specs for job id {pk}')
+    return render(request, 'pages/full-listing.html')
 
 
 def pinned_posts(request):
-    return HttpResponse('Here will be your saved pinned posts')
+    return render(request, 'pages/pinboard.html')
