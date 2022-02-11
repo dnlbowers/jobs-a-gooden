95d8b97b8584ca4c4aff87592215b25fe8173521
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-02-11 19:19:37 +0100

    Change settings py to use sqlite3 in development

--- jobsagooden/settings.py
+++ jobsagooden/settings.py
@@ -85,17 +85,17 @@ WSGI_APPLICATION = 'jobsagooden.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# if "DEVELOPMENT" in os.environ:
-#         DATABASES = {
-#             'default': {
-#                 'ENGINE': 'django.db.backends.sqlite3',
-#                 'NAME': BASE_DIR / 'db.sqlite3',
-#             }
-#         }
-# else:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if "DEVELOPMENT" in os.environ:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
