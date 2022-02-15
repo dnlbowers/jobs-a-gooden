fa07369d6c42225b9b88210fd824540383c3b92f
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-02-16 00:13:52 +0100

    remove sqlite and use solely postres via heroku

--- job_search/urls.py
+++ job_search/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	# Temporarily set jobsearch to home page
-	path('', views.job_search, name='job_search'),
-	path('listing/<str:pk>/', views.full_listing, name='full_listing'),
-	path('pinboard', views.pinned_posts, name='pin_board'),
+    # Temporarily set jobsearch to home page
+    path('', views.job_search, name='job_search'),
+    path('listing/<str:pk>/', views.full_listing, name='full_listing'),
+    path('pinboard', views.pinned_posts, name='pin_board'),
 ]
