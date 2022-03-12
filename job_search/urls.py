db8d6f384dac49889036b66ef5d86a790d2298a8
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-03-12 02:14:03 +0100

    Add title to url and amend head component to take the title from url's page

--- job_search/urls.py
+++ job_search/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     # Temporarily set jobsearch to home page
-    path('', views.JobList.as_view(), name='job_list'),
+    path('', views.JobList.as_view(
+        extra_context={'title': 'Open Positions'}
+        ), name='job_list'),
+    # how do I add title to these views?
     path('<uuid:id>/', views.FullJobSpec.as_view(), name='job_details'),
     path('pinboard', views.pinned_posts, name='pin_board'),
     path('pinned/<uuid:id>/', views.PinJob.as_view(), name='pinned_job')
