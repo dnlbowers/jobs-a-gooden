0bcbd33d10b35cf6517124fb162c3309304fb1d3
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-03-15 01:11:53 +0100

    Add listener for when a new user is created to models.py and reset database

--- job_search/models.py
+++ job_search/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from cloudinary.models import CloudinaryField
 import uuid
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 STATUS = ((0, 'hidden'), (1, 'Public'))
-
 
 class Job(models.Model):
     id = models.UUIDField(
@@ -34,19 +34,20 @@ class Job(models.Model):
         ordering = ['-date_posted']
 
     def __str__(self):
-        return self.company_name + " - " + self.job_title
+        return f'{self.company_name} -  {self.job_title}'
 
 
-# class PinnedJob(models.Model):
-#     job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
-#     is_pinned = models.ManyToManyField(
-#         User, related_name='is_pinned', blank=True
-#     )
+class PinnedJobs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    pinned_jobs = models.ManyToManyField(Job, related_name='pinned_jobs', blank=True)
 
-    # def __str__(self):
-    #     return self.job.company_name + " - " + self.job.job_title + \
-    #         "-" + "pinned by" + self.user.username
+    def __str__(self):
+        return f'{self.user.username}\'s pinned jobs'
 
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        PinnedJobs.objects.create(user=instance)
 
 class Notes(models.Model):
     id = models.UUIDField(
