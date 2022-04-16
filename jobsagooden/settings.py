1142532d61b5a7582641a482c56c89de8640dca9
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-04-16 11:27:27 +0200

    add authentication settings

--- jobsagooden/settings.py
+++ jobsagooden/settings.py
@@ -61,6 +61,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
 ROOT_URLCONF = 'jobsagooden.urls'
 
 TEMPLATES = [
